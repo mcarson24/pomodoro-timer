8066cf9981e22ca311dc01fe0fe69b62ee0c310b
Matthew Carson <matthew24carson@gmail.com>
2019-10-15 18:52:02 -0400

    Complete basic countdown timer

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Button, StyleSheet, Text, Vibration, View } from 'react-native'
+import { vibrate } from './utils/index.js'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      workTimer: 300000,
+      restTimer: 5000,
+      intervalId: ''
+    }
+  }
+  componentDidMount() {
+    // setInterval(this.countdown, 1000)
+  }
+  countdown = () => {
+    const intervalId = setInterval(() => {
+      if (this.state.workTimer === 0) {
+        Vibration.vibrate([500, 500, 500])
+        return
+      }
+      this.setState(prevState => ({
+        workTimer: prevState.workTimer - 1000
+      }))
+    }, 1000)
+    this.setState({intervalId})
+  }
+
+  pause = () => {
+    clearInterval(this.state.intervalId)
+  }
+
+  humanReadableTime = time => {
+    const minutes = Math.floor(time / 1000 / 60)
+    const seconds = (time / 1000 % 60).toString().padStart(2, '0')
+  
+    return `${minutes}:${seconds}`
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Work Timer:</Text>
+        <Text>{this.humanReadableTime(this.state.workTimer)}</Text>
+        <Button title="Start" onPress={this.countdown} />
+        <Button title="Pause" onPress={this.pause} />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
