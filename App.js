4f36f5f2db816c94bf823efaa22833fb2b180943
Matthew Carson <matthew24carson@gmail.com>
2019-10-15 20:07:44 -0400

    Simplify by using one timer under the hood

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, StyleSheet, Text, Vibration, View } from 'react-native'
+import { Button, StyleSheet, TextInput, Text, Vibration, View } from 'react-native'
 import { vibrate } from './utils/index.js'
 
 export default class App extends React.Component {
@@ -8,31 +8,33 @@ export default class App extends React.Component {
     this.state = {
       desiredWorkTime: 5000,
       desiredBreakTime: 5000,
-      activeTimer: 'workTimer',
-      workTimer: 2000,
-      breakTimer: 5000,
-      workTimerIntervalId: '',
-      breakTimerIntervalId: '',
+      currentTime: '',
+      intervalId: ''
     }
+  }
+  componentDidMount() {
+    this.setState(prevState=> ({
+      currentTime: prevState.desiredWorkTime
+    }))
   }
   countdown = () => {
     const intervalId = setInterval(() => {
-      if (this.state[this.state.activeTimer] === 0) {
+      if (this.state.currentTime === 0) {
         Vibration.vibrate([500, 500, 500])
-        clearInterval(this.state[this.state.activeTimer + 'IntervalId'])
+        clearInterval(this.state.intervalId)
         return
       }
       this.setState(prevState => ({
-        [this.state.activeTimer]: prevState[this.state.activeTimer] - 1000
+        currentTime: prevState.currentTime - 1000
       }))
     }, 1000)
     this.setState({
-      [this.state.activeTimer + 'IntervalId']: intervalId
+      intervalId
     })
   }
 
   pause = () => {
-    clearInterval(this.state[this.state.activeTimer + 'IntervalId'])
+    clearInterval(this.state.intervalId)
   }
 
   isActiveTimer = timerName => {
@@ -40,24 +42,22 @@ export default class App extends React.Component {
   }
 
   takeABreak = () => {
-    clearInterval(this.state.activeTimer + 'IntervalId')
+    clearInterval(this.state.intervalId)
     this.setState(prevState => ({
-        activeTimer: 'breakTimer',
-        breakTimer: prevState.desiredBreakTime
+        currentTime: prevState.desiredBreakTime
       }))
   }
 
   backToWork = () => {
-    clearInterval(this.state.activeTimer + 'IntervalId')
-    this.setState(prevstate=> ({
-      activeTimer: 'workTimer',
-      workTimer: prevstate.desiredWorkTime
+    this.state.intervalId
+    this.setState(prevState=> ({
+      workTimer: prevState.desiredWorkTime
     }))
   }
 
-  humanReadableTime = time => {
-    const minutes = Math.floor(time / 1000 / 60)
-    const seconds = (time / 1000 % 60).toString().padStart(2, '0')
+  humanReadableTime = () => {
+    const minutes = Math.floor(this.state.currentTime / 1000 / 60)
+    const seconds = (this.state.currentTime / 1000 % 60).toString().padStart(2, '0')
   
     return `${minutes}:${seconds}`
   }
@@ -65,30 +65,27 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        { this.state.activeTimer === 'workTimer' && (
-          <View>
-            <Text style={styles.timerTitle}>Work Timer:</Text>
-            <Text style={styles.timer}>{this.humanReadableTime(this.state.workTimer)}</Text>
-            { this.state.workTimer === 0 && (
-              <Button title="Take A Break"
-                      onPress={this.takeABreak} /> 
-            )}
-          </View>
-        )}
-        { this.state.activeTimer == 'breakTimer' && (
-          <View>
-            <Text style={styles.timerTitle}>Break Timer:</Text>
-            <Text style={styles.timer}>{this.humanReadableTime(this.state.breakTimer)}</Text>
-            { this.state.breakTimer === 0 && (
-              <Button title="Back To Work"
-                      onPress={this.backToWork} /> 
-            )}
-          </View>
-        )}
+        <View>
+          <Text style={styles.timerTitle}>Work Timer:</Text>
+          <Text style={styles.timer}>{this.humanReadableTime()}</Text>
+          { this.state.currentTime === 0 && (
+            <Button title="Switch Timer"
+                    onPress={this.takeABreak} /> 
+          )}
+        </View>
+        <View>
+          <Text style={styles.timerTitle}>Break Timer:</Text>
+          <Text style={styles.timer}>{this.humanReadableTime()}</Text>
+          { this.state.breakTimer === 0 && (
+            <Button title="Back To Work"
+                    onPress={this.backToWork} /> 
+          )}
+        </View>
         <View style={styles.buttonContainer}>
           <Button title="Start" onPress={this.countdown} />
           <Button title="Pause" onPress={this.pause} />
         </View>
+  
       </View>
     )
   }
